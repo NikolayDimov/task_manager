e2f5159fdc105ce3dd7fda1b20e5c0a8ae01ea26
Nikolay Dimov <dimovv.nikolay@gmail.com>
2025-03-05 16:28:15 +0200

    feat(sidebar) add styles

--- app/Components/Sidebar/Sidebar.style.ts
+++ app/Components/Sidebar/Sidebar.style.ts
@@ -4,6 +4,6 @@ export const SidebarStyles = styled.nav`
     position: relative;
     width: ${(props) => props.theme.sidebarWidth};
     background-color: ${(props) => props.theme.colorBg2};
-    border-right: 2px solid ${(props) => props.theme.borderColor};
+    border: 2px solid ${(props) => props.theme.borderColor};
     border-radius: 1rem;
 `;
--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -2,12 +2,26 @@
 import React from 'react'
 import { SidebarStyles } from './Sidebar.style';
 import { useGlobalState } from '@/app/context/globalProvider';
+import Image from 'next/image';
 
 function Sidebar() {
     const { theme } = useGlobalState();
 
     return (
-        <SidebarStyles theme={theme}>Sidebar</SidebarStyles>
+        <SidebarStyles theme={theme}>
+            <div className="profile">
+                <div className="profile-overlay"></div>
+                <div className="image">
+                    <Image width={70} height={70} src="/avatar.jpg" alt="profile" />
+                </div>
+                <h1>
+                    <span>John</span>
+                    <span>Doe</span>
+                </h1>
+            </div>
+            <ul className="nav-items">
+            </ul>
+        </SidebarStyles>
     )
 }
 
